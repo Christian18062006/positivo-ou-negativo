c2cfdcede9f7f6b7a18a15394e97f707856c8b5d
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-11 22:04:30 -0300

    criada a função para validar se um número é positivo ou negativo e solicitado o número ao usuário.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,23 @@ namespace PositivoNegativo
 {
     class Program
     {
+        public static bool positivoNegativo(int numero)
+        {
+            if (numero>=0)
+            {
+                return (true);
+            }
+            else
+            {
+                return (false);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numero;
+            Console.WriteLine("Positivo ou negativo.");
+            numero = int.Parse(Console.ReadLine());
+
         }
     }
 }
